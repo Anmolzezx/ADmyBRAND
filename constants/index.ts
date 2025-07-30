81c6fa814c0f24a116c7eb8ea7b09744cfcb2dbd
Anmol Singh <anmolsingh7442@gmail.com>
2025-07-31 00:59:30 +0530

    fix: error in icons

--- constants/index.ts
+++ constants/index.ts
@@ -1,5 +1,5 @@
 import instagram from "../public/assets/icons/instagram.png"
-import twitter from "../public/assets/icons/twitter.png"
+import twitter from "../public/assets/icons/X.png"
 import linkedin from "../public/assets/icons/linkedin.png"
 import logo from "../public/assets/logos/WhatsApp Image 2025-07-30 at 14.11.02_96899bb7.jpg"
 import heroImage from "../public/assets/images/57831.jpg"
